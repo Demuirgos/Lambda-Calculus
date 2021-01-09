a0808a8d5618f93745dc1c7d0f313ea3ca787d7a
Ayman Bouchareb <Ayman.bouchareb@outlook.fr>
2021-01-09 17:32:07 +0100

    fixed files

--- Interpreter.fs
+++ Interpreter.fs
@@ -4,44 +4,6 @@ module Interpreter
         | Applicative of Expression * Expression
         | Lambda of string * Expression 
     and Envirement = string * Expression
-<<<<<<< Updated upstream
-        
-    let rec parseAtom =
-        Parser {
-            let pword =['a'..'z'] |> Seq.toList |> anyOf |> many 1 
-            return! pword
-        } <?> "Atom" |>> (toString >> Atom)
-    and parseApp = 
-        Parser {
-            let pParens p = between (expect '(') p (expect ')')
-            let pSpaces = many 1 (expect ' ')
-            let pApp = pParens ( parseExpression .>> pSpaces .>>. parseExpression )
-            return! pApp
-        } <?> "Applicative" |>> Applicative
-    and parseLambda = 
-        Parser {
-            let pLmbda = expect 'λ'
-            let pVar = ['a'..'z'] |> Seq.toList |> anyOf |> many 1 
-            let pDot = expect '.'
-            let pLmbda = pLmbda >>. pVar .>> pDot .>>. parseExpression 
-            return! pLmbda
-        } <?> "Lambda" |>> fun (var,body) -> Lambda (toString var,body)
-    and parseExpression  = 
-        choice [    
-            parseAtom;  
-            parseApp;   
-            parseLambda;    
-        ] <?> "Expression"  
-    let parse expr = 
-        match run expr parseExpression  with 
-        | Success (v)-> sprintf "%A" v
-        | Failure (_) as f -> toResult f 
-=======
-<<<<<<< Updated upstream
-
-    let Parse (input:char list) = 
-        0
-=======
         
     let rec parseAtom =
         Parser {
@@ -82,5 +44,3 @@ module Interpreter
         match run expr parseExpression  with 
         | Success (v)-> sprintf "%A" v
         | Failure (_) as f -> toResult f 
->>>>>>> Stashed changes
->>>>>>> Stashed changes
--- Program.fs
+++ Program.fs
@@ -4,61 +4,6 @@ open Interpreter
 
 [<EntryPoint>]
 let main (args:string []) =
-<<<<<<< Updated upstream
-    // let word = "(-35.3)" 
-    // let pnumber =
-        // let testp p = between (expect '(') p (expect ')') 
-        // let pint = "0123456789" |> Seq.toList |> anyOf |> many 1 
-        // let psign = option (expect '-')
-        // let psep = expect '.'
-        // let pnumber = psign .>>. pint .>>. option (psep .>>. pint)
-        // let mapping = 
-            // let arr2Str chars = chars |> (List.map string) |> List.reduce (+)
-            // function
-            // | ((Some sign,part1),Some (point,part2)) -> 
-                // sprintf "-%s%c%s" (arr2Str part1) (point) (arr2Str part2) |> float
-            // | ((Some sign,part1),None) -> 
-                // sprintf "-%s" (arr2Str part1) |> float
-            // | ((None,part1),Some (point,part2)) -> 
-                // sprintf "%s%c%s" (arr2Str part1) (point) (arr2Str part2) |> float
-            // | ((None,part1),None) -> 
-                // sprintf "%s" (arr2Str part1) |> float
-        // testp pnumber <?> "test"
-        // |> map mapping
-    // (word |> fromStr,pnumber) ||> run  |> toResult |> printf "%A\n"
-    (fromStr "λx.y", parseExpression) 
-    ||> run
-     |> toResult
-     |> printf "%A"
-=======
-<<<<<<< Updated upstream
-    let word = "-35,3" 
-    // let verbParser = Parser {
-    //     let subject = Parser {
-    //         let proc = fun w -> w |> (Seq.toList >> allOf)
-    //         return (proc "the") .>>. expect ' ' .>>. (proc "lake") .>>. expect ' '
-    //     }
-    //     let verb = expect 'i' .>>. expect 's'
-    //     let object = (expect ' ') .>>. (allOf <| Seq.toList "dry" )
-    //     return between subject verb object
-    // }
-    //((Seq.toList word),verbParser) ||> run  |> printf "%A"
-    let pint = 
-        let intp = "0123456789" |> Seq.toList |> anyOf |> many 1 
-        let signp = option (expect '-')
-        let sepp = expect '.'
-        let pnumber = intp .>>. option (sepp .>>. intp)
-        let mapping (((sign,part1),point),part2) = 
-            let arr2Str chars = chars |> (List.map string) |> List.reduce (+) 
-            let absValue = sprintf "%s%c%s" (arr2Str part1) (point) (arr2Str part2) |> float
-            match sign with 
-            | Some _ -> - absValue
-            | _ -> absValue
-        (signp .>>. intp .>>. sepp .>>. intp) <?> "Number"
-        |> map mapping
-    (word |> fromStr,pint) ||> run  |> toResult |> printf "%A"
-    ("ab" |> fromStr) />? (expect 'a') |> printf "%A"
-=======
     Console.OutputEncoding <- Text.Encoding.Unicode
     let word = "(-35.3)" 
     let pnumber =
@@ -85,6 +30,4 @@ let main (args:string []) =
     ||> run
     |> toResult
     |> printf "%s"
->>>>>>> Stashed changes
->>>>>>> Stashed changes
     0
