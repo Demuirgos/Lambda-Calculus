b6269cdb8e67bd0a6a1669d6ef4bcc9d3bf0f8d4
DemuirGos <59487204+DemuirGos@users.noreply.github.com>
2021-01-09 17:25:05 +0100

    Update Interpreter.fs
    added ast parsers with the valid casts
    made them lazy to solve mutual recusive type in a pure functional way

--- Interpreter.fs
+++ Interpreter.fs
@@ -4,6 +4,7 @@ module Interpreter
         | Applicative of Expression * Expression
         | Lambda of string * Expression 
     and Envirement = string * Expression
+<<<<<<< Updated upstream
         
     let rec parseAtom =
         Parser {
@@ -35,3 +36,51 @@ module Interpreter
         match run expr parseExpression  with 
         | Success (v)-> sprintf "%A" v
         | Failure (_) as f -> toResult f 
+=======
+<<<<<<< Updated upstream
+
+    let Parse (input:char list) = 
+        0
+=======
+        
+    let rec parseAtom =
+        Parser {
+            printfn "here in parse atom again"
+            let! atom = ['a'..'z'] |> Seq.toList |> anyOf |> many 1 
+            return atom
+        } <?> "Atom" |>> (toString >> Atom)
+    and parseApp = 
+        Parser {
+            printfn "here in parse applicative again"
+            let pParens p = between (expect '(') p (expect ')')
+            let pSpaces = many 1 (expect ' ')
+            let! app = pParens ( parseExpression .>> pSpaces .>>. parseExpression )
+            return app 
+        } <?> "Applicative" |>> Applicative
+    and parseLambda = 
+        Parser {
+            printfn "here in parse lambda again"
+            let pLmbda = expect 'λ'
+            let pVar = ['a'..'z'] |> Seq.toList |> anyOf |> many 1 
+            let pDot = expect '.'
+            let! lambda = pLmbda >>. pVar .>> pDot .>>. parseExpression 
+            return lambda
+        } <?> "Lambda" |>> fun (var,body) -> Lambda (toString var,body)
+    and parseExpression  = 
+        Parser {
+            printfn "here in parse expr again"
+            let! expr = 
+                choice [    
+                    parseAtom   ;  
+                    parseApp    ;    
+                    parseLambda    
+                ] 
+            return expr
+        } <?> "Expression" 
+         
+    let parse expr = 
+        match run expr parseExpression  with 
+        | Success (v)-> sprintf "%A" v
+        | Failure (_) as f -> toResult f 
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
