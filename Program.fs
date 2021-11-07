c316961236b7bb5a735f05ad50924d109fb74ba3
Demuirgos <Ayman.Bouchareb@etu.uae.ac.ma>
2021-11-07 15:31:38 +0100

    added Read from File

--- Program.fs
+++ Program.fs
@@ -1,19 +1,32 @@
 open System
+open System.IO
+open FSharp.Core
 open Interpreter
 open Abstractor
-
+type Mode = File of string | Terminal
 [<EntryPoint>]
-let REPL _ =
-    Console.OutputEncoding <- Text.Encoding.Unicode
-    let prefix = "λ >"
-    let read = Console.ReadLine >> sprintf "%s"
-    let eval = transpile >> interpret
-    let print= 
-        function 
-        | input ->  printfn "%s" input
-                    prefix      
-    let rec loop prefix = 
-        prefix |> printf "%s"
-        |> read |> eval |> print |> loop
-    loop prefix
+let REPL args =
+    let execute mode= 
+        Console.OutputEncoding <- Text.Encoding.Unicode
+        let prefix = "λ >"
+        let read =  function
+                    | Terminal   -> Console.ReadLine >> sprintf "%s"
+                    | File(path) -> fun () -> [yield! File.ReadLines(path)]
+                                              |> String.concat "\n"
+                                              |> sprintf "%s"
+        let eval = transpile >> interpret
+        let print= 
+            function 
+            | input ->  printfn "%s" input
+                        prefix      
+        let rec loop state prefix =
+            prefix |> printf "%s "
+            |> read(state) |> eval 
+            |> print |> loop Terminal
+        loop mode prefix
+    match args with 
+    | [||]     -> execute <| Terminal
+    | [|path|] -> execute <| File(path)
+    | _        -> failwith "Usage: Can Only Run 1 File at a time"
+
 
