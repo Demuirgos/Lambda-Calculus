737d492d104b5475ccb2aa31b5ae7adc28d52b3d
Demuirgos <Ayman.Bouchareb@etu.uae.ac.ma>
2021-11-20 13:32:57 +0100

    added pure Lambda Mode

--- Program.fs
+++ Program.fs
@@ -3,29 +3,36 @@ open System.IO
 open FSharp.Core
 open Interpreter
 open Abstractor
-type Mode = File of string | Terminal
+type Mode = File of string | Terminal | Lambda
 [<EntryPoint>]
 let REPL args =
     let execute mode= 
         Console.OutputEncoding <- Text.Encoding.Unicode
         let prefix = "λ >"
         let read =  function
-                    | Terminal   -> Console.ReadLine 
                     | File(path) -> fun () -> [yield! File.ReadLines(path)]
                                               |> String.concat "\n"
-        let eval = transpile >> interpret
+                    | _ -> Console.ReadLine 
+        let eval mode = 
+            match mode with 
+            | Lambda -> interpret
+            | _ -> transpile >> interpret
         let print= 
             function 
             | input ->  printfn "%s" input
                         prefix      
         let rec loop state prefix =
             prefix |> printf "%s "
-            |> read(state) |> eval 
-            |> print |> loop Terminal
+            |> read(state) |> eval(state)
+            |> print |> loop (match state with 
+                              | Lambda -> Lambda
+                              | _ -> Terminal )
         loop mode prefix
     match args with 
-    | [||]     -> execute <| Terminal
-    | [|path|] -> execute <| File(path)
-    | _        -> failwith "Usage: Can Only Run 1 File at a time"
+    | [||]       -> execute <| Terminal
+    | [|path|]   -> execute <| File(path)
+    | [|"-lmbd"
+        "true"|] -> execute <| Lambda
+    | _          -> failwith "Usage: Can Only Run 1 File at a time"
 
 
