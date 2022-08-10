12bfd9f1f0316e8f24af3e9dc6b5109617a0d878
Ayman Bouchareb <Ayman.Bouchareb@outlook.fr>
2022-08-10 15:28:00 +0100

    Expended function type checking to include multivariable function defs

--- OxAlc.Service/Typechecker.fs
+++ OxAlc.Service/Typechecker.fs
@@ -17,19 +17,19 @@ module Typechecker
             } <?> "Arrow" |>> Arrow
         parseArrow <|> parseAtom
 
-    let rec typeof term (ctx : TypingContext) = 
+    let rec typeof (ctx : TypingContext) term = 
         match term with 
         | Bind(Identifier(name), tipe, body, cont) -> 
             let ctx = ctx.Add(name, tipe)
-            let bodyType = typeof body ctx
-            let contType = typeof cont ctx
+            let bodyType = typeof ctx body 
+            let contType = typeof ctx cont
             match bodyType, contType with
             | Ok(term_t), Ok(cont_t) when term_t = tipe -> Ok(cont_t)
             | _ -> Error ("Type mismatch") 
         | Branch(_cond, _then, _else) -> 
-            let condType = typeof _cond ctx
-            let thenType = typeof _then ctx
-            let elseType = typeof _else ctx
+            let condType = typeof ctx _cond
+            let thenType = typeof ctx _then
+            let elseType = typeof ctx _else 
             match condType, thenType, elseType with
             | Ok(bool), Ok(then_t), Ok(else_t) when then_t = else_t -> Ok(then_t)
             | _ -> Error ("Type mismatch")
@@ -37,16 +37,28 @@ module Typechecker
             if(ctx.ContainsKey(name)) 
             then Ok ctx[name]
             else Error "Type error"
-        | Function([(Identifier(name), in_t)], out_v) -> 
-            let ctx = ctx.Add(name, in_t)
-            match typeof out_v ctx with 
-            | Ok(out_t) -> Ok <| Arrow(in_t, out_t)
+        | Function(in_vs, out_v) ->
+            let rec addRange l ctx: TypingContext = match l with 
+                | [] -> ctx
+                | (Identifier(name), arg_t):: t -> addRange t (ctx.Add(name, arg_t))  
+            let ctx = addRange in_vs ctx
+            match typeof ctx  out_v with 
+            | Ok(out_t) -> 
+                let rec constructType = 
+                    function
+                    | [] -> failwith "Type error"
+                    | [(_, arg_t)] -> Arrow(arg_t, out_t)
+                    | (_, arg_t):: t ->  Arrow(arg_t, constructType t)
+                Ok <| constructType in_vs
             | _ -> Error "Type error"
-        | Application(func, [arg])  -> 
-            let func_t = typeof func ctx
-            let arg_t = typeof arg ctx
-            match func_t, arg_t with
-            | Ok(Arrow(in_t, out_t)), Ok(arg_t) when arg_t = in_t -> Ok out_t
-            | _ -> Error "Type error" 
-
+        | Application(func, args)  -> 
+            let func_t = typeof ctx func 
+            let args_t = args |> List.map (typeof ctx) 
+            let rec checkValidity func args = 
+                match func, args with
+                | Ok t , [] -> Ok (t)
+                | Ok (Arrow(in_t, out_t)), (Ok t):: ts 
+                    when in_t = t -> checkValidity (Ok out_t) ts
+                | _, (Error _):: _ | Error _, _ | _ -> Error "Type error"
+            checkValidity func_t args_t
         
