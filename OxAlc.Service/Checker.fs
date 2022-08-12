795c07692d4232f057ea2022e42101f1e37fbac5
Ayman Bouchareb <Ayman.Bouchareb@outlook.fr>
2022-08-12 02:39:01 +0100

    Fixed custom operator type checking, Added somewhat meaningfull error (WIP)

--- OxAlc.Service/Checker.fs
+++ OxAlc.Service/Checker.fs
@@ -21,30 +21,33 @@ module Typechecker
                     if suggested_type = Atom "" 
                     then TypeOf (ctx.Add(name, term_t)) cont 
                     else TypeOf ctx cont
-            | _ -> 
-                Error ("Type mismatch") 
+            | Ok(term_t) -> Error (sprintf "Type mismatch in bind : expected type %s but given type %s" (suggested_type.ToString()) (term_t.ToString())) 
+            | error -> error 
         | Branch(_cond, _then, _else) -> 
             let condType = TypeOf ctx _cond
             let thenType = TypeOf ctx _then
             let elseType = TypeOf ctx _else 
             match condType, thenType, elseType with
             | Ok(Atom "bool"), Ok(then_t), Ok(else_t) when then_t = else_t -> Ok(then_t)
-            | _ -> Error ("Type mismatch")
+            | Ok(Atom "bool"), Ok(then_t), Ok(else_t) -> 
+                Error (sprintf "Type mismatch in if-expr : expression branches types don't match %s and %s" (then_t.ToString()) (else_t.ToString())) 
+            | Ok cond_t, _, _ -> Error (sprintf "Type mismatch in if-expr : Condition Type must be %s but given %s" (Atom("bool").ToString()) (cond_t.ToString())) 
+            | Error err, _, _ | _, Error err, _ | _, _ , Error err -> Error err
         | Identifier(name)  -> 
             if(ctx.ContainsKey(name)) 
             then Ok ctx[name]
-            else Error "Type error"
+            else Error (sprintf "Undefined identifier %s" name)
         | Function(in_vs, out_v) ->
             let ctx = addRange in_vs ctx
             match TypeOf ctx  out_v with 
             | Ok(out_t) -> 
-                let rec constructType = 
+                let rec constructType res = 
                     function
-                    | [] -> failwith "Type error"
-                    | [(_, arg_t)] -> Arrow(arg_t, out_t)
-                    | (_, arg_t):: t ->  Arrow(arg_t, constructType t)
-                Ok <| constructType in_vs
-            | _ -> Error "Type error"
+                    | [] -> Error "Function expects at least one argument"
+                    | [(_, arg_t)] -> Ok (Arrow(arg_t, res))
+                    | (_, arg_t):: t ->  constructType (Arrow(arg_t, res)) t
+                in_vs |> List.rev |> constructType out_t
+            | error -> error
         | Application(func, args)  -> 
             let func_t = TypeOf ctx func 
             let args_t = args |> List.map (TypeOf ctx) 
@@ -53,7 +56,10 @@ module Typechecker
                 | Ok t , [] -> Ok (t)
                 | Ok (Arrow(in_t, out_t)), (Ok t):: ts 
                     when in_t = t -> checkValidity (Ok out_t) ts
-                | _, (Error _):: _ | Error _, _ | _ -> Error "Type error"
+                | Ok (Arrow(in_t, _)), (Ok t):: ts -> 
+                    Error (sprintf "Type mismatch in application : expected type %s but given type %s" (in_t.ToString()) (t.ToString()))
+                | _, (Error err):: _ | Error err, _ -> Error err
+                | _ -> Error "Type mismatch in application"
             checkValidity func_t args_t
         | Compound(expr, binds) -> 
             let rec typesList binds res = 
@@ -62,13 +68,14 @@ module Typechecker
                 | ((iden, id_t), body) :: t -> 
                     match TypeOf ctx body with
                     | Ok type_r when type_r = id_t -> typesList t ((iden, id_t)::res) 
-                    | _ -> Error "Type error"
+                    | Ok type_r -> Error (sprintf "Type mismatch in compound : expected type %s but given type %s" (id_t.ToString()) (type_r.ToString()))
+                    | Error err -> Error err
             let bindTypes = typesList binds []
             match bindTypes with
             | Ok binds_t -> 
                 let ctx = addRange binds_t ctx
                 TypeOf ctx expr
-            | _ -> Error "Type error" 
+            | Error msg -> Error msg 
         | Value lit -> 
             match lit with 
             | Variable _ -> Ok <| Atom "number"
@@ -80,7 +87,7 @@ module Typechecker
             let left_t = TypeOf ctx left
             let right_t = TypeOf ctx right
             match left_t, right_t with
-            | Ok (left_t), Ok (right_t) when left_t = right_t -> 
+            | Ok (left_t), Ok (right_t) -> 
                 match op, left_t, right_t with
                 | Add , Atom "number" , Atom "number" -> Ok (Atom "number")
                 | Subs, Atom "number" , Atom "number" -> Ok (Atom "number")
@@ -95,15 +102,20 @@ module Typechecker
                 | Not , Atom "bool"   , Atom "bool"   -> Ok (Atom "bool")
                 | Xor , Atom "bool"   , Atom "bool"   -> Ok (Atom "bool")
                 | Cons, Atom _        , Atom "list"   -> Ok (Atom "list")
-                | Custom symbol, lhs_t, rhs_t -> 
+                | Custom symbol, loperand_t, roperand_t -> 
                     if(ctx.ContainsKey(symbol)) then 
                         let typeOfExpr = 
-                            Function ([(left, left_t); (right, right_t)] , Identifier symbol) 
-                            |> TypeOf ctx 
+                            let typeOfBinaryOp = TypeOf ctx (Identifier symbol) 
+                            match typeOfBinaryOp with
+                            | Ok (Arrow(lhs_t, Arrow(rhs_t, out_t))) 
+                                when lhs_t = loperand_t && rhs_t = roperand_t-> Ok out_t
+                            | Ok (Arrow(lhs_t, Arrow(rhs_t, out_t)) as binop_t) 
+                                -> Error (sprintf "Type mismatch in binary operation : expected type %s but given type %s" (binop_t.ToString()) (Arrow(loperand_t, Arrow(roperand_t, out_t)).ToString())) 
+                            | error -> error 
                         typeOfExpr
-                    else Error "Type error" 
-                | _ -> Error "Type error"
-            | _ -> Error "Type error"
+                    else Error (sprintf "Undefined binary operation %s" symbol)
+                | _ -> Error "Unsupported binary operation"
+            | Error msg, _ | _, Error msg -> Error msg 
 
 
         // Statement * ((Statement * Type) * Statement) list
